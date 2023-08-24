086b4abedcb2425b36181861a21ee03e1632820a
Sayed Zulfikar <74494579+sayedsayedsayed@users.noreply.github.com>
2023-08-24 10:38:58 +0700

    Initial Commit

---
+++ Demo-NavigationStack/Demo_NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_NavigationStackApp.swift
+//  Demo-NavigationStack
+//
+//  Created by Sayed Zulfikar on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Demo_NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
